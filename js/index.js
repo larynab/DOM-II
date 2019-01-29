67d9b0f1e20322b66b423ce22689684e35544230
larynab <fsovreign@gmail.com>
2019-01-29 13:20:15 -0800

    eventlistener-beta

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+   //`mouseover`
+const mouseOverA = document.querySelector("nav");
+mouseOverA.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "orange";
+        setTimeout(function() {
+        event.target.style.color = "red";
+        }, 500);
+  }, false);
+
+const mouseEnterA = document.querySelector('.destination h4') 
+mouseEnterA.addEventListener("mouseenter", function( event ) {   
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "red";
+    }, 500);
+  }, false);
+  
+
+/*
+	* [ ] `keydown`
+	* [ ] `wheel`
+	* [ ] `drag / drop`
+	* [ ] `load`
+	* [ ] `focus`
+	* [ ] `resize`
+	* [ ] `scroll`
+	* [ ] `select`
+	`dblclick`
+*/
