e3d3727ae6602becc78e8c8679156f0abe6f20bd
larynab <fsovreign@gmail.com>
2019-01-29 15:03:10 -0800

    prevent-prop-default

--- js/index.js
+++ js/index.js
@@ -14,6 +14,15 @@ mouseEnterA.addEventListener("mouseenter", function( event ) {
     setTimeout(function() {
       event.target.style.color = "red";
       }, 500);
+    event.stopPropagation();  
+  }, false);
+const mouseEnterB = document.querySelector('.content-section'); 
+mouseEnterB.addEventListener("mouseenter", function( event ) {   
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "red";
+      }, 500);
+    event.stopPropagation();  
   }, false);
 
 //focus
@@ -23,33 +32,50 @@ blurA.addEventListener('focus', function( event ) {
 }, true);
 
 //Blur
-blurA.addEventListener('blur', function( event ) {
-  event.target.style.background = 'silver';    
+blurA.addEventListener('blur', e => {
+  e.target.style.background = 'silver';    
 }, true);
 
 //scroll
-
 let last_known_scroll_position = 0;
 let ticking = false;
 
 function doSomething(scroll_pos) {
-  const colorChangeA = document.querySelector('p');
-  colorChangeA.style.color = 'pink';// Do something with the scroll position
+  const colorChangeA = document.querySelector('body');
+  colorChangeA.style.color = 'pink';
+  
 }
-
 window.addEventListener('scroll', function(e) {
   last_known_scroll_position = window.scrollY;
-
-  if (!ticking) {
+  
+    
+    if (!ticking) {
     window.requestAnimationFrame(function() {
       doSomething(last_known_scroll_position);
       ticking = false;
+       
     });
-
     ticking = true;
+    
   }
+e.stopPropagation(); 
 });
 
+
+//Load
+  window.addEventListener("load", function(event) {
+    const colorChangeB = document.querySelector('body');
+    colorChangeB.style.color = 'blue';
+    console.log("IT FINISHED LOADING OH MY GOODNESS");
+  });
+
+//stopPropagtion
+//event.stopPropagation();
+
+//Prevent Default
+  document.querySelector("nav").addEventListener("click", function(event) {
+         event.preventDefault();
+}, false);
 
 
 
